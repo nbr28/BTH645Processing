ec85f56dd3789d6cbf575f06bf8161602b42ac76
Natan Ross <natanross@hotmail.com>
2019-04-10 18:26:45 -0400

    Done

--- sketch_shapes.js
+++ sketch_shapes.js
@@ -1,21 +1,56 @@
 /* sketch_shape.js */
 
 var testImg;
+var StarArray = [];
+var angle = 0;
+
 function preload() {
-    testImg = loadImage('moon.png');
-  }
-  
+    testImg = loadImage('assets/moon.png');
+}
+
 
 function setup() {
     createCanvas(600, 400);
     background(0);
+    frameRate(15);
+    for (i = 0; i < 100; i++)
+        StarArray.push(new Star());
+}
+
+function nextRad(theta) {
+    let r = height * 0.50;
+    var cordiantes = { x: 0, y: 0 };
+    cordiantes.x = r * cos(theta);
+    cordiantes.y = abs(r * sin(theta))*-1;
+
+    return cordiantes;
 }
 
 function draw() {
 
+    clear();//for each frame
+
+    background(0);
     noStroke();
 
-    image(testImg, 0, 0);
+
+    push();
+    var moonPoint = nextRad(angle);
+    angle += 0.001;
+    translate(width / 2, height / 2);
+    image(testImg, moonPoint.x, moonPoint.y, 50, 50);
+    pop();
+
+
+    //stars
+   
+    for (i = 0; i < 100; i++) {
+        push();
+        StarArray[i].display();
+        pop();
+    }
+    
+
     push();
     {
         fill(255, 204, 0);
@@ -57,6 +92,26 @@ function draw() {
     pop();
 
 }
+
+function Star() {
+
+    this.Brighness = random() * 255;
+    this.x = random(width)
+    this.y = abs(random(height));
+    this.diameter = 1;
+}
+
+Star.prototype.display = function () {
+
+    noStroke();
+    fill(color(this.Brighness,this.Brighness,this.Brighness));
+    ellipse(this.x, this.y, 2, 2);
+
+    this.Brighness +=random(-50,50);
+}
+
+
+
 
 
 
